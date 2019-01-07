dabd9311c9cc5247e0883b5b1bef15201db6b172
f8B23Lef <mal7rish@yandex.ru>
2019-01-08 02:38:51 +0300

    refactor: update readmi + change config files

--- game/.eslintrc.js
+++ game/.eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   "extends": "airbnb-base",
   "env": {
-      "browser": true,
-      "node": true,
-      "jest": true
+    "browser": true,
+    "node": true,
+    "jest": true
   },
   rules: {
-      "linebreak-style": 0
+    "linebreak-style": 0,
+    "import/no-cycle": [2, { maxDepth: 1 }]
   },
 };
